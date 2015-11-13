172fec407139f88bfb89d74978d1dd8a80551fa0
Victor <victormel87@gmail.com>
2015-11-13 11:17:15 +0100

    Creado metodo getDisplayValue para mostrarnos el valor de nuestro display

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -27,13 +27,26 @@ public class NumberDisplay
      */
     public void setValue(int valorDisplay){
         // Si el valor pasado como parametro es menor o igual que el limite establecido en el constructor entonces le asignamos al atributo el valor introducido.
-        if(valorDisplay <= limite){
+        if((valorDisplay <= limite) && (valorDisplay >= 0)){
             valor = valorDisplay;
         }
         else{
             // Si el valor introducido es mas grande que el limite entonces indicamos al usuario un mensaje de error.
-            System.out.println("El valor introducido:" + valorDisplay + " se sale del limite del display:" + limite);
+            System.out.println("El valor introducido:" + valorDisplay + " se sale de los limites del display [0 - " + limite + "]");
         }
         
+    }
+    
+    /**
+     * Metodo para obtener un string con los digitos introducidos.
+     */
+    public String getDisplayValue(){
+        // Creamos una variable local para almacenar nuestro valor del display.
+        String digitos = "" + valor;
+        // si el valor es menor que 10 entonces tendremos que añadirle un digito delante de nuestro valor.
+        if(valor < 10){
+            digitos = "0" + valor;
+        }
+        return digitos;
     }
 }
