403d583056d64871c4516aec97e22d0c45aac751
Victor <victormel87@gmail.com>
2015-11-13 10:51:03 +0100

    Creado el contructor NumberDisplay con un parametro limite

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -7,27 +7,18 @@
  */
 public class NumberDisplay
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Atributo que difinira el valor del digito.
+    private int valor;
+    // Atributo que definira el limite del valor.
+    private int limite;
 
     /**
-     * Constructor for objects of class NumberDisplay
+     * Constructor para crear displays con un valor y un limite pasado como parametro.
      */
-    public NumberDisplay()
+    public NumberDisplay(int limiteDisplay)
     {
-        // initialise instance variables
-        x = 0;
+        valor = 0;
+        limite = limiteDisplay;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
