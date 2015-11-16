9d6724dee3d0cfc6717719159a824491bdd35a62
Victor <victormel87@gmail.com>
2015-11-16 10:36:00 +0100

    Añadida clase ClockDisplay

---
+++ ClockDisplay.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        
+    }
+
+}
