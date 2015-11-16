b36cb52885856e5255690471c369a9b88f4c9337
Victor <victormel87@gmail.com>
2015-11-16 10:39:35 +0100

    Añadidos el atributo de tipo String tiempoFinal y añadidos comentarios

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -7,8 +7,12 @@
  */
 public class ClockDisplay
 {
+    // Atributo de tipo NumberDisplay para las horas
     private NumberDisplay horas;
+    // Atributo de tipo NumberDisplay para los minutos
     private NumberDisplay minutos;
+    // Atributo de tipo String para guardar la hora completa.
+    private String horaFinal;
 
     /**
      * Constructor for objects of class ClockDisplay
